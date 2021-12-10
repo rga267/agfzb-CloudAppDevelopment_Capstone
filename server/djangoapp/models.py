d7063c45959ad88a34f973e7c3ae270e2d9f5908
Ricardo Gutierrez <rga267@nyu.edu>
2021-12-10 13:28:09 -0500

    added models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,6 +4,77 @@ from django.utils.timezone import now
 
 # Create your models here.
 
+class CarMake(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    description = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.name+" "+self.description
+
+
+class CarModel(models.Model):
+    SEDAN = 'sedan'
+    SUV = 'suv'
+    WAGON = 'wagon'
+    CAR_TYPE = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon')
+    ]
+    carMake = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    dealerId = models.IntegerField(null=False)
+    name = models.CharField(max_length=100, null=False)
+    type_of_vehicle = models.CharField(max_length=100, choices=CAR_TYPE, null=False)
+    year = models.DateField(null=False)
+
+    def __str__(self):
+        return self.carMake+" , "+self.name+" , "+self.year
+
+class CarDealer:
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, state, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        self.state = state
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        #return "Address: " + self.address
+        return "Dealer name: " + self.full_name
+
+# <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview:
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, id):
+        self.dealership = dealership
+        self.name = name
+        self.purchase = purchase
+        self.review = review
+        self.purchase_date = purchase_date
+        self.car_make = car_make
+        self.car_model = car_model
+        self.car_year = car_year
+        self.sentiment = ""
+        self.id = id
+
+    def __str__(self):
+        return "Dealership: " + self.dealership 
+
+
+
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
 # - Description
